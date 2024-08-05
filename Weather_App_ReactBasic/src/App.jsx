a40c127b581ab5a69de3747caaccfe3bc0dc8307
Nannita1403 <nannimagi@gmail.com>
2024-08-05 16:38:08 +0200

    .

--- Weather_App_ReactBasic/src/App.jsx
+++ Weather_App_ReactBasic/src/App.jsx
@@ -1,25 +1,21 @@
 
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import './App.css'
 import Header from './components/Header/Header'
-import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
-import NotFound from './pages/NotFound/NotFound'
-import WeatherTodayPage from './pages/WeatherTodayPage/WeatherTodayPage'
-import WeekForecastPage from './pages/WeekForecastPage/WeekForecastPage'
+import Main from './pages/Main/Main'
 
 const App = () => {
   return (
-    <div>
+    <main
+    style={{
+      width: '100%',
+      height: '100vh'
+    }}>
       <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/WeatherToday" element={<WeatherTodayPage/>}></Route>
-        <Route path="/WeekForecast" element={<WeekForecastPage />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Main/>
       <Footer/>
-    </div>
+    </main>
   )
 }
 
--- Weather_App_ReactBasic/src/main.jsx
+++ Weather_App_ReactBasic/src/main.jsx
@@ -1,11 +1,24 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CityProvider } from './context/CityContext.jsx'
+import Home from './pages/Home/Home.jsx'
+import WeatherTodayPage from './pages/WeatherTodayPage/WeatherTodayPage.jsx'
+import WeekForecastPage from './pages/WeekForecastPage/WeekForecastPage.jsx'
+import NotFound from './pages/NotFound/NotFound.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-  <App /> 
+ <BrowserRouter basename='/'>
+ <CityProvider>
+     <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route index element={<Home />}></Route>
+        <Route path="WeatherToday" element={<WeatherTodayPage />}></Route>
+        <Route path="WeekForecast" element={<WeekForecastPage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+  </CityProvider>
 </BrowserRouter>
 )
 
